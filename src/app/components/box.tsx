7362672a0baa627a1e7449884a9bdcd295cba607
Maksym-W <maksymwoychyshyn@gmail.com>
2025-07-27 23:21:57 -0400

    Small typo fix

--- src/app/components/box.tsx
+++ src/app/components/box.tsx
@@ -10,7 +10,7 @@ interface BoxProps {
 export default function Box({ children, bgColor = "bg-green-200", pageNumber = -1, status = "Empty" }: BoxProps) {
   return (
     <div className={`p-6 ${bgColor} text-black rounded-box shadow-md border border-green-600`}>
-      <h2 className="text-lg font-bold mb-2">Page Number: {pageNumber}</h2>
+      <h2 className="text-lg font-bold mb-2">Page: {pageNumber}</h2>
       <p className="mb-4">The Status of this is: {status}</p>
       {children}
     </div>
