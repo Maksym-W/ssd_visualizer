fe3abe7c05b14a4aaf617b0da180d5628498c5ca
Maksym-W <maksymwoychyshyn@gmail.com>
2025-08-04 16:16:38 -0400

    Stale pages are now tracked

--- src/app/components/ssdblock.tsx
+++ src/app/components/ssdblock.tsx
@@ -13,9 +13,18 @@ interface SsdblockProps extends BoxProps {
 }
 
 export default function Ssdblock({ pages,  blockNumber,  startPageIndex, bgColour = "bg-green-200", status = "Empty", blockSize }: SsdblockProps) {
+  const [numOfStalePages, setNumOfStalePages] = useState(-1); // The value of kilobytes the user wants to write.
+
+  useEffect(() => {
+    let stales = 0; // This will be used to update numOfStalePages
+    for (const page of pages) {if (page.status != "empty") {stales++;}} // THIS BETTER THIS WAY!!!!!
+    console.log(stales);
+    setNumOfStalePages(stales);
+  }, [pages]); 
+
   return (
     <div className="bg-blue-50 rounded-lg p-3 shadow-inner">
-      <h3 className="text-center font-bold mb-2">Block {blockNumber}</h3>
+      <h3 className="text-center font-bold mb-2">Block: {blockNumber}. Valid pages: {16 - numOfStalePages}</h3>
       <div className={`grid grid-cols-${Math.floor(blockSize / 4)} gap-2`}>
         {[...Array(blockSize)].map((_, i) => {
           const pageIndex = startPageIndex + i;
