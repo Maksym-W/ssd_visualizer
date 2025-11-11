2acab0ede135e22093a072d0121a9f3cde58ae75
clarks03 <stephen.clark@mail.utoronto.ca>
2025-11-11 18:36:35 -0500

    added dots on blank ssd pages for accessibility

--- src/app/components/ssdpage.tsx
+++ src/app/components/ssdpage.tsx
@@ -2,7 +2,7 @@ import { Page } from "../../lib/Page";
 
 export default function SSDPage({ page, pageNumber, pageIndex }: { page: Page, pageNumber?: number, pageIndex?: number }) {
   let status = (pageNumber) ? `${pageNumber} (${pageIndex})` : ''
-  status = (page.status.startsWith("Stale")) ? "X" : status;
+  status = (page.status.startsWith("Stale")) ? "X" : (page.status.startsWith("Empty") ? "." : status);
   return (
     <div className={`${page.bgColour} border border-green-700 flex items-center justify-center text-xs text-black`}>
       <p>{status}</p>
